cf118d896e255596fb3bbf1c4811abda95319a85
A.L <xenide@users.noreply.github.com>
2022-09-20 16:53:30 -0500

    test: set up test

--- test/unit/ReservoirRouterTest.sol
+++ test/unit/ReservoirRouterTest.sol
@@ -2,6 +2,16 @@ pragma solidity 0.8.13;
 
 import "v3-core/test/__fixtures/BaseTest.sol";
 
+import { WETH } from "solmate/tokens/WETH.sol";
+
+import { ReservoirRouter } from "src/ReservoirRouter.sol";
+
 contract ReservoirRouterTest is BaseTest {
 
+    ReservoirRouter private _router = new ReservoirRouter(address(_factory), address(0));
+    WETH            private _weth   = new WETH();
+
+    function testHaha() external {
+
+    }
 }
