8483422221edff7d194a03041bbb0f01e3c36888
A.L <xenide@users.noreply.github.com>
2022-10-01 19:02:02 -0500

    refactor: updated variable names in library

--- src/libraries/ReservoirLibrary.sol
+++ src/libraries/ReservoirLibrary.sol
@@ -15,179 +15,179 @@ library ReservoirLibrary {
     uint256 public constant FEE_ACCURACY  = 10_000;
 
     // returns sorted token addresses, used to handle return values from pairs sorted in this order
-    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-        require(tokenA != tokenB, "RL: IDENTICAL_ADDRESSES");
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), "RL: ZERO_ADDRESS");
+    function sortTokens(address aTokenA, address aTokenB) internal pure returns (address rToken0, address rToken1) {
+        require(aTokenA != aTokenB, "RL: IDENTICAL_ADDRESSES");
+        (rToken0, rToken1) = aTokenA < aTokenB ? (aTokenA, aTokenB) : (aTokenB, aTokenA);
+        require(rToken0 != address(0), "RL: ZERO_ADDRESS");
     }
 
     // calculates the CREATE2 address for a pair without making any external calls
-    function pairFor(address factory, address tokenA, address tokenB, uint256 curveId) internal pure returns (address pair) {
-        (address token0, address token1) = sortTokens(tokenA, tokenB);
+    function pairFor(address aFactory, address aTokenA, address aTokenB, uint256 aCurveId) internal pure returns (address rPair) {
+        (address lToken0, address lToken1) = sortTokens(aTokenA, aTokenB);
 
         bytes memory lInitCode;
 
-        if (curveId == 0) {
-            lInitCode = abi.encodePacked(type(ConstantProductPair).creationCode, abi.encode(token0, token1));
+        if (aCurveId == 0) {
+            lInitCode = abi.encodePacked(type(ConstantProductPair).creationCode, abi.encode(lToken0, lToken1));
         }
-        else if (curveId == 1) {
-            lInitCode = abi.encodePacked(type(StablePair).creationCode, abi.encode(token0, token1));
+        else if (aCurveId == 1) {
+            lInitCode = abi.encodePacked(type(StablePair).creationCode, abi.encode(lToken0, lToken1));
         }
         else {
             revert("RL: CURVE_DOES_NOT_EXIST");
         }
 
-        pair = address(uint160(uint256(keccak256(abi.encodePacked(
+        rPair = address(uint160(uint256(keccak256(abi.encodePacked(
             bytes1(0xff),
-            factory,
+            aFactory,
             bytes32(0),
             keccak256(lInitCode)
         )))));
     }
 
-    function getSwapFee(address factory, address tokenA, address tokenB, uint256 curveId) internal view returns (uint swapFee) {
-        swapFee = IReservoirPair(pairFor(factory, tokenA, tokenB, curveId)).swapFee();
+    function getSwapFee(address aFactory, address aTokenA, address aTokenB, uint256 aCurveId) internal view returns (uint rSwapFee) {
+        rSwapFee = IReservoirPair(pairFor(aFactory, aTokenA, aTokenB, aCurveId)).swapFee();
     }
 
     // does not support tokens with > 18 decimals
-    function getPrecisionMultiplier(address token) internal view returns (uint64 precisionMultiplier) {
-        precisionMultiplier = uint64(10 ** (18 - ERC20(token).decimals()));
+    function getPrecisionMultiplier(address aToken) internal view returns (uint64 rPrecisionMultiplier) {
+        rPrecisionMultiplier = uint64(10 ** (18 - ERC20(aToken).decimals()));
     }
 
     // returns the precise amplification coefficient for calculation purposes
-    function getAmplificationCoefficient(address pair) internal view returns (uint64 amplificationCoefficient) {
-        amplificationCoefficient = StablePair(pair).getCurrentAPrecise();
+    function getAmplificationCoefficient(address aPair) internal view returns (uint64 rAmplificationCoefficient) {
+        rAmplificationCoefficient = StablePair(aPair).getCurrentAPrecise();
     }
 
     // fetches and sorts the reserves for a pair
     function getReserves(
-        address factory,
-        address tokenA,
-        address tokenB,
-        uint256 curveId
-    ) internal view returns (uint256 reserveA, uint256 reserveB) {
-        (address token0,) = sortTokens(tokenA, tokenB);
-        (uint reserve0, uint reserve1,) = IReservoirPair(pairFor(factory, tokenA, tokenB, curveId)).getReserves();
-        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+        address aFactory,
+        address aTokenA,
+        address aTokenB,
+        uint256 aCurveId
+    ) internal view returns (uint256 rReserveA, uint256 rReserveB) {
+        (address lToken0,) = sortTokens(aTokenA, aTokenB);
+        (uint lReserve0, uint lReserve1,) = IReservoirPair(pairFor(aFactory, aTokenA, aTokenB, aCurveId)).getReserves();
+        (rReserveA, rReserveB) = aTokenA == lToken0 ? (lReserve0, lReserve1) : (lReserve1, lReserve0);
     }
 
     // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
     // this works for both ConstantProduct and Stable pairs
-    function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
-        require(amountA > 0, "RL: INSUFFICIENT_AMOUNT");
-        require(reserveA > 0 && reserveB > 0, "RL: INSUFFICIENT_LIQUIDITY");
-        amountB = amountA * reserveB / reserveA;
+    function quote(uint aAmountA, uint aReserveA, uint aReserveB) internal pure returns (uint rAmountB) {
+        require(aAmountA > 0, "RL: INSUFFICIENT_AMOUNT");
+        require(aReserveA > 0 && aReserveB > 0, "RL: INSUFFICIENT_LIQUIDITY");
+        rAmountB = aAmountA * aReserveB / aReserveA;
     }
 
     function computeStableLiquidity(
-        uint256 reserve0,
-        uint256 reserve1,
-        uint256 token0PrecisionMultiplier,
-        uint256 token1PrecisionMultiplier,
+        uint256 aReserve0,
+        uint256 aReserve1,
+        uint256 aToken0PrecisionMultiplier,
+        uint256 aToken1PrecisionMultiplier,
         uint256 N_A
     ) internal pure returns (uint256) {
         return StableMath._computeLiquidityFromAdjustedBalances(
-            reserve0 * token0PrecisionMultiplier,
-            reserve1 * token1PrecisionMultiplier,
+            aReserve0 * aToken0PrecisionMultiplier,
+            aReserve1 * aToken1PrecisionMultiplier,
             N_A
         );
     }
 
     function getAmountOutConstantProduct(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut,
-        uint256 swapFee
-    ) internal pure returns (uint256 amountOut) {
-        require(amountIn > 0, "RL: INSUFFICIENT_INPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
-        uint amountInWithFee = amountIn * (FEE_ACCURACY - swapFee);
-        uint numerator = amountInWithFee * reserveOut;
-        uint denominator = reserveIn * FEE_ACCURACY + amountInWithFee;
-        amountOut = numerator / denominator;
+        uint256 aAmountIn,
+        uint256 aReserveIn,
+        uint256 aReserveOut,
+        uint256 aSwapFee
+    ) internal pure returns (uint256 rAmountOut) {
+        require(aAmountIn > 0, "RL: INSUFFICIENT_INPUT_AMOUNT");
+        require(aReserveIn > 0 && aReserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
+        uint lAmountInWithFee = aAmountIn * (FEE_ACCURACY - aSwapFee);
+        uint lNumerator = lAmountInWithFee * aReserveOut;
+        uint lDenominator = aReserveIn * FEE_ACCURACY + lAmountInWithFee;
+        rAmountOut = lNumerator / lDenominator;
     }
 
     function getAmountInConstantProduct(
-        uint256 amountOut,
-        uint256 reserveIn,
-        uint256 reserveOut,
-        uint256 swapFee
-    ) internal pure returns (uint256 amountIn) {
-        require(amountOut > 0, "RL: INSUFFICIENT_OUTPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
-        uint numerator = reserveIn * amountOut * FEE_ACCURACY;
-        uint denominator = (reserveOut - amountOut) * (FEE_ACCURACY - swapFee);
-        amountIn = (numerator / denominator) + 1;
+        uint256 aAmountOut,
+        uint256 aReserveIn,
+        uint256 aReserveOut,
+        uint256 aSwapFee
+    ) internal pure returns (uint256 rAmountIn) {
+        require(aAmountOut > 0, "RL: INSUFFICIENT_OUTPUT_AMOUNT");
+        require(aReserveIn > 0 && aReserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
+        uint lNumerator = aReserveIn * aAmountOut * FEE_ACCURACY;
+        uint lDenominator = (aReserveOut - aAmountOut) * (FEE_ACCURACY - aSwapFee);
+        rAmountIn = (lNumerator / lDenominator) + 1;
     }
 
     function getAmountOutStable(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut,
-        uint256 swapFee,
-        ExtraData memory data
-    ) internal pure returns (uint256 amountOut) {
-        require(amountIn > 0, "RL: INSUFFICIENT_INPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
+        uint256 aAmountIn,
+        uint256 aReserveIn,
+        uint256 aReserveOut,
+        uint256 aSwapFee,
+        ExtraData memory aData
+    ) internal pure returns (uint256 rAmountOut) {
+        require(aAmountIn > 0, "RL: INSUFFICIENT_INPUT_AMOUNT");
+        require(aReserveIn > 0 && aReserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
 
-        amountOut = StableMath._getAmountOut(
-            amountIn,
-            reserveIn,
-            reserveOut,
-            data.token0PrecisionMultiplier,
-            data.token1PrecisionMultiplier,
+        rAmountOut = StableMath._getAmountOut(
+            aAmountIn,
+            aReserveIn,
+            aReserveOut,
+            aData.token0PrecisionMultiplier,
+            aData.token1PrecisionMultiplier,
             true,
-            swapFee,
-            2 * data.amplificationCoefficient
+            aSwapFee,
+            2 * aData.amplificationCoefficient
         );
     }
 
     function getAmountInStable(
-        uint256 amountOut,
-        uint256 reserveIn,
-        uint256 reserveOut,
-        uint256 swapFee,
-        ExtraData memory data
-    ) internal pure returns (uint256 amountIn) {
-        require(amountOut > 0, "RL: INSUFFICIENT_OUTPUT_AMOUNT");
-        require(reserveIn > 0 && reserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
+        uint256 aAmountOut,
+        uint256 aReserveIn,
+        uint256 aReserveOut,
+        uint256 aSwapFee,
+        ExtraData memory aData
+    ) internal pure returns (uint256 rAmountIn) {
+        require(aAmountOut > 0, "RL: INSUFFICIENT_OUTPUT_AMOUNT");
+        require(aReserveIn > 0 && aReserveOut > 0, "RL: INSUFFICIENT_LIQUIDITY");
 
-        amountIn = StableMath._getAmountIn(
-            amountOut,
-            reserveIn,
-            reserveOut,
-            data.token0PrecisionMultiplier,
-            data.token1PrecisionMultiplier,
+        rAmountIn = StableMath._getAmountIn(
+            aAmountOut,
+            aReserveIn,
+            aReserveOut,
+            aData.token0PrecisionMultiplier,
+            aData.token1PrecisionMultiplier,
             false,
-            swapFee,
-            2 * data.amplificationCoefficient
+            aSwapFee,
+            2 * aData.amplificationCoefficient
         );
     }
 
     // performs chained getAmountOut calculations on any number of pairs
     function getAmountsOut(
-        address factory,
-        uint256 amountIn,
-        address[] calldata path,
-        uint256[] calldata curveIds
-    ) internal view returns (uint256[] memory amounts) {
-        require(path.length >= 2, "RL: INVALID_PATH");
-        require(curveIds.length == path.length - 1, "RL: CURVE_IDS_INVALID_LENGTH");
-        amounts = new uint[](path.length);
-        amounts[0] = amountIn;
-        for (uint i = 0; i < path.length - 1; ) {
-            (uint reserveIn, uint reserveOut) = getReserves(factory, path[i], path[i + 1], curveIds[i]);
-            uint swapFee = getSwapFee(factory, path[i], path[i + 1], curveIds[i]);
-            if (curveIds[i] == 0) {
-                amounts[i + 1] = getAmountOutConstantProduct(amounts[i], reserveIn, reserveOut, swapFee);
+        address aFactory,
+        uint256 aAmountIn,
+        address[] calldata aPath,
+        uint256[] calldata aCurveIds
+    ) internal view returns (uint256[] memory rAmounts) {
+        require(aPath.length >= 2, "RL: INVALID_PATH");
+        require(aCurveIds.length == aPath.length - 1, "RL: CURVE_IDS_INVALID_LENGTH");
+        rAmounts = new uint[](aPath.length);
+        rAmounts[0] = aAmountIn;
+        for (uint i = 0; i < aPath.length - 1; ) {
+            (uint lReserveIn, uint lReserveOut) = getReserves(aFactory, aPath[i], aPath[i + 1], aCurveIds[i]);
+            uint lSwapFee = getSwapFee(aFactory, aPath[i], aPath[i + 1], aCurveIds[i]);
+            if (aCurveIds[i] == 0) {
+                rAmounts[i + 1] = getAmountOutConstantProduct(rAmounts[i], lReserveIn, lReserveOut, lSwapFee);
             }
-            else if (curveIds[i] == 1) {
-                ExtraData memory data = ExtraData(
-                    uint64(getPrecisionMultiplier(path[i])),
-                    uint64(getPrecisionMultiplier(path[i + 1])),
-                    uint64(getAmplificationCoefficient(pairFor(factory, path[i], path[i + 1], 1)))
+            else if (aCurveIds[i] == 1) {
+                ExtraData memory lData = ExtraData(
+                    uint64(getPrecisionMultiplier(aPath[i])),
+                    uint64(getPrecisionMultiplier(aPath[i + 1])),
+                    uint64(getAmplificationCoefficient(pairFor(aFactory, aPath[i], aPath[i + 1], 1)))
                 );
-                amounts[i + 1] = getAmountOutStable(amounts[i], reserveIn, reserveOut, swapFee, data);
+                rAmounts[i + 1] = getAmountOutStable(rAmounts[i], lReserveIn, lReserveOut, lSwapFee, lData);
             }
             unchecked { i += 1; }
         }
@@ -195,28 +195,28 @@ library ReservoirLibrary {
 
     // performs chained getAmountIn calculations on any number of pairs
     function getAmountsIn(
-        address factory,
-        uint256 amountOut,
-        address[] calldata path,
-        uint256[] calldata curveIds
-    ) internal view returns (uint[] memory amounts) {
-        require(path.length >= 2, "RL: INVALID_PATH");
-        require(curveIds.length == path.length - 1, "RL: CURVE_IDS_INVALID_LENGTH");
-        amounts = new uint[](path.length);
-        amounts[amounts.length - 1] = amountOut;
-        for (uint i = path.length - 1; i > 0; ) {
-            (uint reserveIn, uint reserveOut) = getReserves(factory, path[i - 1], path[i], curveIds[i - 1]);
-            uint swapFee = getSwapFee(factory, path[i - 1], path[i], curveIds[i - 1]);
-            if (curveIds[i - 1] == 0) {
-                amounts[i - 1] = getAmountInConstantProduct(amounts[i], reserveIn, reserveOut, swapFee);
+        address aFactory,
+        uint256 aAmountOut,
+        address[] calldata aPath,
+        uint256[] calldata aCurveIds
+    ) internal view returns (uint[] memory rAmounts) {
+        require(aPath.length >= 2, "RL: INVALID_PATH");
+        require(aCurveIds.length == aPath.length - 1, "RL: CURVE_IDS_INVALID_LENGTH");
+        rAmounts = new uint[](aPath.length);
+        rAmounts[rAmounts.length - 1] = aAmountOut;
+        for (uint i = aPath.length - 1; i > 0; ) {
+            (uint lReserveIn, uint lReserveOut) = getReserves(aFactory, aPath[i - 1], aPath[i], aCurveIds[i - 1]);
+            uint lSwapFee = getSwapFee(aFactory, aPath[i - 1], aPath[i], aCurveIds[i - 1]);
+            if (aCurveIds[i - 1] == 0) {
+                rAmounts[i - 1] = getAmountInConstantProduct(rAmounts[i], lReserveIn, lReserveOut, lSwapFee);
             }
-            else if (curveIds[i - 1] == 1) {
-                ExtraData memory data = ExtraData(
-                    uint64(getPrecisionMultiplier(path[i - 1])),
-                    uint64(getPrecisionMultiplier(path[i])),
-                    uint64(getAmplificationCoefficient(pairFor(factory, path[i], path[i - 1], 1)))
+            else if (aCurveIds[i - 1] == 1) {
+                ExtraData memory lData = ExtraData(
+                    uint64(getPrecisionMultiplier(aPath[i - 1])),
+                    uint64(getPrecisionMultiplier(aPath[i])),
+                    uint64(getAmplificationCoefficient(pairFor(aFactory, aPath[i], aPath[i - 1], 1)))
                 );
-                amounts[i - 1] = getAmountInStable(amounts[i], reserveIn, reserveOut, swapFee, data);
+                rAmounts[i - 1] = getAmountInStable(rAmounts[i], lReserveIn, lReserveOut, lSwapFee, lData);
             }
             unchecked{ i -= 1; }
         }
