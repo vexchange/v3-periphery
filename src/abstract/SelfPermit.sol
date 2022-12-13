f685a30df55623e08631e2613034740184edc665
A.L <xenide@users.noreply.github.com>
2022-12-14 00:10:29 +0800

    chore: fix import to get it to compile

--- src/abstract/SelfPermit.sol
+++ src/abstract/SelfPermit.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/drafts/IERC20Permit.sol";
+import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol";
+import { IERC20Permit } from "@openzeppelin/token/ERC20/extensions/draft-IERC20Permit.sol";
 
-import "../interfaces/ISelfPermit.sol";
-import "../interfaces/external/IERC20PermitAllowed.sol";
+import { ISelfPermit } from "src/interfaces/ISelfPermit.sol";
+import { IERC20PermitAllowed } from "src/interfaces/IERC20PermitAllowed.sol";
 
 /// @title Self Permit
 /// @notice Functionality to call permit on any EIP-2612-compliant token for use in the route
